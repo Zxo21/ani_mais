f050a70c01cea3d1ad70125e58f8c29ee98f2ce3
Juan Victor <106358011+Zxo21@users.noreply.github.com>
2025-05-16 08:08:54 -0300

    Update conexao.php

--- web&api/conexao.php
+++ web&api/conexao.php
@@ -5,9 +5,5 @@
     $dbName = 'ani_mais';
 
     $con = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-/*
-    mysqli_query($con, "SET NAMES 'utf8'") or die("Erro na SQL" . mysqli_error($con));
-    mysqli_query($con, 'SET character_set_connection=utf8') or die("Erro na SQL" . mysqli_error($con));
-    mysqli_query($con, 'SET character_set_client=utf8') or die("Erro na SQL" . mysqli_error($con));
-    mysqli_query($con, 'SET character_set_results=utf8') or die("Erro na SQL" . mysqli_error($con));*/
+
 ?>
